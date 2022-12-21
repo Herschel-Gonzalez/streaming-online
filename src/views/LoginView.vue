c6b1faa9221d034b66ebb4149f59164950fc59e5
Herschel-Gonzalez <contacto@herschelgonzalez.com>
2022-12-20 18:34:54 -0600

    version inicial

---
+++ src/views/LoginView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    
+    <input type="mail" placeholder="Correo">
+    <input type="password" placeholder="Contraseña">
+    <button>Iniciar Sesión</button>
+  </main>
+</template>
---
+++ src/views/RegistroView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <input type="text" placeholder="Nombre">
+    <input type="text" placeholder="Apellido Paterno">
+    <input type="text" placeholder="Apellido Materno">
+    <input type="number" placeholder="Edad">
+    <input type="date" placeholder="Fecha de nacimiento">
+    Sexo<select name="sexo" id="sexo">
+        <option value="Masculino">Masculino</option>
+        <option value="Femenino">Femenino</option>
+    </select>
+    <select name="genero" id="genero">
+        <option value=""></option>
+    </select>
+    <button>Registrar</button>
+  </main>
+</template>
