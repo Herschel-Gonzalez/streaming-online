54dc5e23a12c0e682916d9bcfcfc97926c58832b
Herschel-Gonzalez <contacto@herschelgonzalez.com>
2022-12-28 23:57:57 -0600

    se arreglo bug de main

--- src/main.js
+++ src/main.js
@@ -1,28 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
 
 import './assets/main.css'
 
-import { initializeApp } from 'firebase/app';
+let app = null
 
-//import { getAuth } from 'firebase/auth';
+firebase.auth().onAuthStateChanged(()=>{
+    if (!app) {
+       app = createApp(App);
+       app.use(router)
 
-
-const firebaseApp = initializeApp({
-  apiKey: "AIzaSyCATKfTENZDI-7hvsP5hT5M1rOcWAX1eXk",
-  authDomain: "streaming-online-8d51d.firebaseapp.com",
-  projectId: "streaming-online-8d51d",
-  storageBucket: "streaming-online-8d51d.appspot.com",
-  messagingSenderId: "14790765745",
-  appId: "1:14790765745:web:9a54a9b51c41a0dd0e8690"
-});
-
-const auth = getAuth(firebaseApp);
-
-const app = createApp(App)
-
-
-app.use(router)
-
-app.mount('#app')
+       app.mount('#app')
+    }
+})
